0226387c3d85d00977dae3f88dadbaf00474d1ff
Bruce Eckel <BruceTEckel@gmail.com>
2017-08-22 16:44:32 -0600

    Rewrote "Data Classes"

--- Examples/dataClasses/CopyDataClass.kt
+++ Examples/dataClasses/CopyDataClass.kt
@@ -2,21 +2,21 @@
 import atomicTest.eq
 
 data class DetailedContact(
-    val name: String,
-    val surname: String,
-    val number: String,
-    val address: String
+  val name: String,
+  val surname: String,
+  val number: String,
+  val address: String
 )
 
 fun main(args: Array<String>) {
   val contact = DetailedContact(
-      "Miffy",
-      "Miller",
-      "1-234-567890",
-      "1600 Amphitheatre Parkway")
+    "Miffy",
+    "Miller",
+    "1-234-567890",
+    "1600 Amphitheatre Parkway")
   val newContact = contact.copy(
-      number = "098-765-4321",
-      address = "Brandschenkestrasse 110")
+    number = "098-765-4321",
+    address = "Brandschenkestrasse 110")
   newContact eq DetailedContact(
     "Miffy",
     "Miller",
--- Examples/secondaryConstructors/GardenGnome.kt
+++ Examples/secondaryConstructors/GardenGnome.kt
@@ -4,28 +4,24 @@ import atomicTest.eq
 class GardenGnome(
     val height: Double,
     val weight: Double,
-    val happy: Boolean
-) {
+    var happy: Boolean) {
   init {
-    println("Inside primary constructor")
+    println("In primary constructor")
   }
-
   var painted = true
-  fun magic(level: Int): String = "Poof! " + level
-
-  constructor(height: Double)                // [1]
-      : this(height, 100.0, true)
-
-  constructor(name: String) : this(15.0) {   // [2]
-    painted eq true
+  fun magic(level: Int) = "Poof! $level"
+  constructor(height: Double):     // [1]
+    this(height, 100.0, true)
+  constructor(happy: Boolean): this(15.0) {
+    this.happy = happy             // [2]
   }
-
-  fun show(): String = "$height $weight $happy $painted"
+  fun show() =
+    "$height $weight $happy $painted"
 }
 
 fun main(args: Array<String>) {
-  GardenGnome(20.0, 110.0, false).
-      show() eq "20.0 110.0 false true"
-  GardenGnome("Bob").show() eq
-      "15.0 100.0 true true"
+  GardenGnome(20.0, 110.0, false).show() eq
+    "20.0 110.0 false true"
+  GardenGnome(happy=true).show() eq
+    "15.0 100.0 true true"
 }
