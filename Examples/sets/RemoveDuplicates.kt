bd3e6cd006eee15c9ab562026ab85b6dd1c8bba2
Bruce Eckel <BruceTEckel@gmail.com>
2017-08-23 05:03:19 -0600

    "Sets" rewritten

--- Examples/sets/RemoveDuplicates.kt
+++ Examples/sets/RemoveDuplicates.kt
@@ -4,6 +4,5 @@ import atomicTest.eq
 fun main(args: Array<String>) {
   val list = listOf(3, 3, 2, 1, 2)
   list.toSet() eq setOf(1, 2, 3)
-
   "abbcc".toSet() eq setOf('a', 'b', 'c')
 }
--- Examples/sets/Sets.kt
+++ Examples/sets/Sets.kt
@@ -4,9 +4,9 @@ import atomicTest.eq
 fun main(args: Array<String>) {
   val set = setOf(1, 1, 2, 3, 9, 9, 4)
   // No duplicates:
-  set eq setOf(1, 9, 2, 3, 4)
+  set eq setOf(1, 2, 3, 4, 9)
 
-  // Order of elements is not important:
+  // Element order is unimportant:
   setOf(1, 2) eq setOf(2, 1)
 
   // Set membership:
@@ -16,20 +16,20 @@ fun main(args: Array<String>) {
   set.contains(9) eq true
   set.contains(99) eq false
 
-  // Is this set contains another one?
+  // Does this set contain another set?
   set.containsAll(setOf(1, 9, 2)) eq true
 
   // Set union:
   set.union(setOf(3, 4, 5, 6)) eq
-      setOf(1, 2, 3, 4, 5, 6, 9)
+    setOf(1, 2, 3, 4, 5, 6, 9)
 
   // Set intersection:
   set intersect setOf(0, 1, 2, 7, 8) eq
-      setOf(1, 2)
+    setOf(1, 2)
 
   // Set difference:
   set - setOf(0, 1, 9, 10) eq
-      setOf(2, 3, 4)
+    setOf(2, 3, 4)
   set.minus(setOf(0, 1, 9, 10)) eq
-      setOf(2, 3, 4)
+    setOf(2, 3, 4)
 }
