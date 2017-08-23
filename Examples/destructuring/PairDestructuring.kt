bd3445ea08f445b2c7cc633f76694af63c69286b
Bruce Eckel <BruceTEckel@gmail.com>
2017-08-23 06:05:11 -0600

    Rewrote "Destructuring Declarations"

--- Examples/destructuring/PairDestructuring.kt
+++ Examples/destructuring/PairDestructuring.kt
@@ -1,11 +1,11 @@
-// destructuringDeclarations/PairsAndDesctructuring.kt
+// destructuring/PairDestructuring.kt
 import atomicTest.eq
 
 fun compute2(input: Int): Pair<Int, String> =
-    if (input > 5)
-      Pair(input * 2, "High")
-    else
-      Pair(input * 2, "Low")
+  if (input > 5)
+    Pair(input * 2, "High")
+  else
+    Pair(input * 2, "Low")
 
 fun main(args: Array<String>) {
   val (value, description) = compute2(7)
--- Examples/destructuring/Pairs.kt
+++ Examples/destructuring/Pairs.kt
@@ -1,11 +1,11 @@
-// destructuringDeclarations/Pairs.kt
+// destructuring/Pairs.kt
 import atomicTest.eq
 
 fun compute1(input: Int) =
-    if (input > 5)
-      Pair(input * 2, "High")
-    else
-      Pair(input * 2, "Low")
+  if (input > 5)
+    Pair(input * 2, "High")
+  else
+    Pair(input * 2, "Low")
 
 fun main(args: Array<String>) {
   compute1(7) eq Pair(14, "High")
