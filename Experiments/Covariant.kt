7a931e7c74b370537f16cb25d67f45970dd8f832
Bruce Eckel <mindviewinc@gmail.com>
2017-10-04 12:41:37 -0600

    Better

--- Experiments/Covariant.kt
+++ Experiments/Covariant.kt
@@ -2,6 +2,6 @@ open class Animal
 class Cat: Animal()
 
 fun main(args: Array<String>) {
-  val cats = listOf(Cat(), Cat())
+  val cats: List<Cat> = listOf(Cat(), Cat())
   val animals: List<Animal> = cats
 }
