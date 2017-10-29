d040200fa93917a1fdefc253a77510847da8d5de
Bruce Eckel <mindviewinc@gmail.com>
2017-10-28 17:37:56 -0700

    In the midst of changing to tailrec

--- Examples/PropertyDelegation/FibonacciProperty.kt
+++ Examples/PropertyDelegation/FibonacciProperty.kt
@@ -12,6 +12,21 @@ fun fibonacci(n: Int): Int {
   }
 }
 
+class Fibonacci {
+  private var current = 0
+  operator fun getValue(delegator: Any?,
+    prop: KProperty<*>) = current
+  operator fun setValue(delegator: Any?,
+    prop: KProperty<*>, n: Int) {
+    current = fibonacci(n)
+  }
+}
+
 fun main(args: Array<String>) {
-  println(fibonacci(22))
+  var f by Fibonacci()
+  f eq 0
+  f = 22
+  f eq 17711
+  f = 23
+  f eq 28657
 }
