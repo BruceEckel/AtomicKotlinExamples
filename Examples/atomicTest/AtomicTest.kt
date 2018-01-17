42f36f9e5b75a7a293ec8b921adc30d406f4b4b3
Bruce Eckel <mindviewinc@gmail.com>
2018-01-16 18:41:31 -0700

    Updated

--- Examples/atomicTest/AtomicTest.kt
+++ Examples/atomicTest/AtomicTest.kt
@@ -1,121 +1,104 @@
 // AtomicTest/AtomicTest.kt
-/* A minimal test framework for the book, to
+/*
+A minimal test framework for the book, to
 display results and introduce & promote unit
 testing early in the learning curve.
 */
 package atomictest
-import java.util.*
-import java.io.*
+import kotlin.math.abs
 
-private val errTag = "[Error]:"
-private val expectedException =
-  "$errTag Expected an exception"
+private val errorTag = "[Error]:"
+private val expectedExceptionMessage =
+  "$errorTag Expected an exception"
 
-private fun <L, R> test(display: String,
-  actual: L, expected: R,
-  failTest: () -> Boolean,
-  altMsg: String = "") {
-  println(display)
-  if(failTest())
-    if(altMsg == "")
-      println("$errTag $actual != $expected")
-    else
-      println(altMsg)
+private fun <L, R> runTest(
+  actual: L,
+  expected: R,
+  checkEquals: Boolean = true,
+  test: () -> Boolean
+) {
+  println(actual)
+  if (!test()) {
+    print(errorTag)
+    val message: String =
+      if (checkEquals)
+        "$actual != $expected"
+      else
+        "$actual == $expected"
+    println(message)
+  }
 }
 
-// Compare as Strings. Removes all whitespace
-// to allow embedded linefeeds & indentation:
-infix fun <T: Any> T.eq(rv: String) {
+/**
+ * Compares string representation
+ * of the object with the string `value`.
+ * Removes all whitespace to allow embedded
+ * linefeeds & indentation.
+ */
+infix fun <T : Any> T.eq(value: String) {
   val ws = "\\s".toRegex()
-  val lval = this.toString().replace(ws, "")
-  val rval = rv.toString().replace(ws, "")
-  test(this.toString(), lval, rval,
-    { lval.compareTo(rval) != 0 })
+  fun String.rmws() = this.replace(ws, "")
+  runTest(this, value) {
+    this.toString().rmws() == value.rmws()
+  }
 }
 
+/**
+ * Checks that object is equal to `value`.
+ */
 infix fun <T> T.eq(value: T) {
-  test(this.toString(), this, value,
-    { this != value })
+  runTest(this, value) {
+    this == value
+  }
 }
 
+/**
+ * Checks that `Double` number is equal
+ * to `value` within a positive delta.
+ */
 infix fun Double.eq(value: Double) {
-  test(this.toString(), this, value,
-    { Math.abs(this - value) > 0.0000001 })
+  runTest(this, value) {
+    abs(this - value) < 0.0000001
+  }
 }
 
+/**
+ * Checks that object isn't equal to `value`.
+ */
 infix fun <T> T.neq(value: T) {
-  test(this.toString(), this, value,
-    { this == value },
-    "$errTag $this == $value")
+  runTest(this, value, checkEquals = false) {
+    this != value
+  }
 }
 
-// Capture an exception and produce its name.
-// Usage:
-//   capture {
-//     // Code that fails
-//   } eq "FailureException"
+/**
+ * Captures an exception and produces its name.
+ * Usage:
+ *   ```
+ *   capture {
+ *     // Code that fails
+ *   } eq "FailureException"
+ *   ```
+ */
 fun capture(f: () -> Unit): String =
   try {
     f()
-    expectedException
-  } catch(e: Throwable) {
-    e.javaClass.simpleName
-  }
-
-// Capture a stack trace for comparison.
-// Ignore whitespace and line numbers.
-// Usage:
-//   stacktrace {
-//     // Code that fails
-//   } eq """(stack trace)"""
-object stacktrace {
-  private var result = ""
-  operator fun
-  invoke(f: () -> Unit): stacktrace {
-    result = try {
-      f()
-      expectedException
-    } catch(e: Throwable) {
-      val trace = StringWriter()
-      e.printStackTrace(PrintWriter(trace))
-      trace.toString()
-    }
-    return this
-  }
-  infix fun eq(rv: String) {
-    val ws = "\\s".toRegex()
-    val lineNum = ":\\d+".toRegex()
-    fun String.clean() = this
-      .replace(ws, "").replace(lineNum, "")
-    val lval = result.clean()
-    val rval = rv.clean()
-    test(result.toString(), lval, rval,
-      { lval.compareTo(rval) != 0 },
-      "$errTag\n[$lval]\n!=\n[$rval]")
-  }
-}
-
-// Capture first line of stack trace,
-// and extract just the essence:
-fun stacktrace1(f: () -> Unit): String =
-  try {
-    f()
-    expectedException
-  } catch(e: Throwable) {
-    val trace = StringWriter()
-    e.printStackTrace(PrintWriter(trace))
-    trace.toString().lines().first()
-      .replaceBefore(
-        ":", e.javaClass.simpleName)
+    expectedExceptionMessage
+  } catch (e: Throwable) {
+    e.javaClass.simpleName +
+      (e.message?.let { ": $it" } ?: "")
   }
 
 // Add messages via trace(msg). Validate with:
 // trace eq "(trace contents)"
 object trace {
   private var result = ""
-  operator fun invoke(msg: Any) {
-    result += msg.toString() + "\n"
+  operator fun invoke(message: Any) {
+    result += message.toString() + "\n"
   }
-  fun reset() { result = "" }
-  infix fun eq(rv: String) = result eq rv
+  fun reset() {
+    result = ""
+  }
+  infix fun eq(value: String) =
+    result eq value
 }
