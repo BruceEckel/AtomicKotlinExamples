ebf0686255a113976d200a1332724c6f83f8dd70
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2018-01-18 19:24:32 +0100

    Formatting; regenerated tests

--- Tests/AbstractTestExamples.kt
+++ Tests/AbstractTestExamples.kt
@@ -5,39 +5,40 @@ import java.io.PrintStream
 import java.util.function.Consumer
 
 abstract class AbstractTestExamples {
-    protected fun testExample(fileName: String, main: Consumer<Array<String>>) {
-        val exampleCode = File(fileName).readText()
-        if (exampleCode.contains("/* Output:")) {
-            val output = extractOutput(exampleCode)
-            testOutput(fileName, output, main)
-        }
-        testAtomicChecks(fileName, main)
+  protected fun testExample(fileName: String, main: Consumer<Array<String>>) {
+    val exampleCode = File(fileName).readText()
+    if (exampleCode.contains("/* Output:")) {
+      val output = extractOutput(exampleCode)
+      testOutput(fileName, output, main)
     }
+    testAtomicChecks(fileName, main)
+  }
 
-    private fun extractOutput(exampleCode: String) =
-            exampleCode.substringAfter("/* Output:").substringBefore("*/").trim()
+  private fun extractOutput(exampleCode: String) =
+      exampleCode.substringAfter("/* Output:").substringBefore("*/").trim()
 
-    private fun String.trimAndNormalizeLineSeparators() = trim().replace("\\R".toRegex(), System.getProperty("line.separator"))
+  private fun String.trimAndNormalizeLineSeparators() =
+      trim().replace("\\R".toRegex(), System.getProperty("line.separator"))
 
-    private fun testOutput(fileName: String, expectedOutput: String, main: Consumer<Array<String>>) {
-        val actualOutput = runAndGetOutput(main)
-        Assert.assertEquals("Incorrect output for '$fileName':\n",
-                expectedOutput.trimAndNormalizeLineSeparators(),
-                actualOutput.trimAndNormalizeLineSeparators())
-    }
+  private fun testOutput(fileName: String, expectedOutput: String, main: Consumer<Array<String>>) {
+    val actualOutput = runAndGetOutput(main)
+    Assert.assertEquals("Incorrect output for '$fileName':\n",
+        expectedOutput.trimAndNormalizeLineSeparators(),
+        actualOutput.trimAndNormalizeLineSeparators())
+  }
 
-    private fun runAndGetOutput(main: Consumer<Array<String>>): String {
-        val out = ByteArrayOutputStream()
-        System.setOut(PrintStream(out))
+  private fun runAndGetOutput(main: Consumer<Array<String>>): String {
+    val out = ByteArrayOutputStream()
+    System.setOut(PrintStream(out))
 
-        main.accept(arrayOf())
+    main.accept(arrayOf())
 
-        val toString = out.toString()
-        return toString
-    }
+    val toString = out.toString()
+    return toString
+  }
 
-    private fun testAtomicChecks(fileName: String, main: Consumer<Array<String>>) {
-        val output = runAndGetOutput(main)
-        Assert.assertFalse("AtomicTest checks failed for '$fileName':\n$output", output.contains("[Error]:"))
-    }
+  private fun testAtomicChecks(fileName: String, main: Consumer<Array<String>>) {
+    val output = runAndGetOutput(main)
+    Assert.assertFalse("AtomicTest checks failed for '$fileName':\n$output", output.contains("[Error]:"))
+  }
 }
--- Tests/generating/GenerateTests.kt
+++ Tests/generating/GenerateTests.kt
@@ -3,75 +3,75 @@ package generating
 import java.io.File
 
 fun main(args: Array<String>) {
-    generateExampleTests()
+  generateExampleTests()
 }
 
 fun generateExampleTests() {
-    val examples = File("Examples")
-    val exampleFiles = examples.listFiles().flatMap { atom ->
-        atom.listFiles().filter { it.extension == "kt" }.map {
-            ExampleInfo.create(it)
-        }
+  val examples = File("Examples")
+  val exampleFiles = examples.listFiles().flatMap { atom ->
+    atom.listFiles().filter { it.extension == "kt" }.map {
+      ExampleInfo.create(it)
     }
-    val tests = generateTests(exampleFiles)
-    File("Tests/TestExamples.java").writeText(tests)
+  }
+  val tests = generateTests(exampleFiles)
+  File("Tests/TestExamples.java").writeText(tests)
 }
 
 data class ExampleInfo(
-        val file: File,
-        val name: String,
-        val packageName: String?
+    val file: File,
+    val name: String,
+    val packageName: String?
 ) {
-    val classForFileName: String
-        get() = name + "Kt"
+  val classForFileName: String
+    get() = name + "Kt"
 
-    val qualifiedName: String
-        get() = if (packageName != null) "$packageName.$classForFileName" else classForFileName
+  val qualifiedName: String
+    get() = if (packageName != null) "$packageName.$classForFileName" else classForFileName
 
-    companion object {
-        fun create(file: File): ExampleInfo {
-            val name = file.nameWithoutExtension
-            val lines = file.readLines()
-            val packageName = lines.find { it.startsWith("package ") }?.substringAfter("package ")?.trim()
-            return ExampleInfo(file, name, packageName)
-        }
+  companion object {
+    fun create(file: File): ExampleInfo {
+      val name = file.nameWithoutExtension
+      val lines = file.readLines()
+      val packageName = lines.find { it.startsWith("package ") }?.substringAfter("package ")?.trim()
+      return ExampleInfo(file, name, packageName)
     }
+  }
 }
 
 fun generateTests(exampleInfoList: List<ExampleInfo>): String {
-    val namesFrequency = mutableMapOf<String, Int>()
-    val tests = mutableListOf<String>()
-    for (exampleInfo in exampleInfoList) {
-        if (!exampleInfo.file.readText().contains("fun main")) continue
+  val namesFrequency = mutableMapOf<String, Int>()
+  val tests = mutableListOf<String>()
+  for (exampleInfo in exampleInfoList) {
+    if (!exampleInfo.file.readText().contains("fun main")) continue
 
-        val classForFileName = exampleInfo.classForFileName
-        val index = if (classForFileName in namesFrequency) {
-            val frequency = namesFrequency.getValue(classForFileName)
-            namesFrequency[classForFileName] = frequency + 1
-            "$frequency"
-        } else {
-            namesFrequency[classForFileName] = 1
-            ""
-        }
-        val exampleName = """test${exampleInfo.name.capitalize()}$index"""
+    val classForFileName = exampleInfo.classForFileName
+    val index = if (classForFileName in namesFrequency) {
+      val frequency = namesFrequency.getValue(classForFileName)
+      namesFrequency[classForFileName] = frequency + 1
+      "$frequency"
+    } else {
+      namesFrequency[classForFileName] = 1
+      ""
+    }
+    val exampleName = """test${exampleInfo.name.capitalize()}$index"""
 
-        tests += """
+    tests += """
             @Test
             public void $exampleName() {
-                testExample("${(exampleInfo.file.path).replace("\\", "/")}", ${exampleInfo.qualifiedName}::main);
-            }""".replaceIndent("    ")
-    }
+              testExample("${(exampleInfo.file.path).replace("\\", "/")}", ${exampleInfo.qualifiedName}::main);
+            }""".replaceIndent("  ")
+  }
 
-    return buildString {
-        appendln("import org.junit.Test;")
-        appendln()
-        appendln("""
+  return buildString {
+    appendln("import org.junit.Test;")
+    appendln()
+    appendln("""
                 public class TestExamples extends AbstractTestExamples {
                 """.trimIndent())
-        for (test in tests) {
-            appendln()
-            appendln(test)
-        }
-        append("}\n")
+    for (test in tests) {
+      appendln()
+      appendln(test)
     }
+    append("}\n")
+  }
 }
