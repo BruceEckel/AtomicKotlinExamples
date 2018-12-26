83ff50c5d90b0c55074b9292c24ae1916232c795
Bruce Eckel <mindviewinc@gmail.com>
2018-12-26 15:03:32 -0700

    Update Contravariance.kt

--- Examples/Variance/Contravariance.kt
+++ Examples/Variance/Contravariance.kt
@@ -16,30 +16,13 @@ class Derived2 : Derived() {
     "Derived2::f"
 }
 
-fun contraTest(b: Derived2) {
-  b.f(Mallard()) eq "Base::f"
-  b.f(Duck()) eq "Derived::f"
-  b.f(Bird()) eq "Derived2::f"
-}
-
 fun main() {
-  val base = Base()
-  val derived = Derived()
-  val derived2 = Derived2()
+  Derived2().f(Mallard()) eq "Base::f"
+  Derived2().f(Duck()) eq "Derived::f"
+  Derived2().f(Bird()) eq "Derived2::f"
 
-  val bird = Bird()
-  val duck = Duck()
-  val mallard = Mallard()
+  Derived().f(Mallard()) eq "Base::f"
+  Derived().f(Duck()) eq "Derived::f"
 
-  derived2.f(mallard) eq "Base::f"
-  derived2.f(duck) eq "Derived::f"
-  derived2.f(bird) eq "Derived2::f"
-
-  derived.f(mallard) eq "Base::f"
-  derived.f(duck) eq "Derived::f"
-  // derived.f(bird) eq "Derived2::f"
-
-  base.f(mallard) eq "Base::f"
-  // base.f(duck) eq "Derived::f"
-  // base.f(bird) eq "Derived2::f"
+  Base().f(Mallard()) eq "Base::f"
 }
