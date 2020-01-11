13cc1ddb90af390ebbae82e9cf63597e952e750f
Bruce Eckel <mindviewinc@gmail.com>
2020-01-10 17:28:15 -0800

    Incorporated factory into Stage for cleaner design

--- Examples/ObjectOrientedDesign/Players.kt
+++ Examples/ObjectOrientedDesign/Players.kt
@@ -1,7 +1,5 @@
 // ObjectOrientedDesign/Players.kt
 package robotexplorer1
-import robotexplorer1.Player.*
-import robotexplorer1.Urge.*
 
 class Robot(var room: Room) {
   fun move(urge: Urge) {
@@ -27,30 +25,4 @@ class Teleport(val target: Char) {
   var originRoom = Room()
   var targetRoom = Room()
   override fun toString() = target.toString()
-}
-
-fun factory(ch: Char): Any {
-  if(ch == 'R')
-    return Robot(edge)
-  Player.values().forEach {
-    if (ch == it.symbol)
-      return it
-  }
-  return Teleport(ch)
-}
-
-fun factory2(ch: Char, room: Room, robot: Robot) {
-  if(ch == 'R') {
-    robot.room = room
-    return
-  }
-  Player.values().forEach {
-    if (ch == it.symbol) {
-      room.player = it
-      return
-    }
-  }
-  val teleport = Teleport(ch)
-  room.player = teleport
-  teleport.originRoom = room
 }
--- Examples/ObjectOrientedDesign/Stage.kt
+++ Examples/ObjectOrientedDesign/Stage.kt
@@ -8,53 +8,46 @@ class Stage(val maze: String) {
     mutableMapOf<Pair<Int, Int>, Room>()
   private val view = View(this)
   val lines = maze.split("\n")
+  fun factory(ch: Char): Room {
+    val room = Room()
+    if(ch == 'R') {
+      robot.room = room
+      return room
+    }
+    Player.values().forEach {
+      if (ch == it.symbol) {
+        room.player = it
+        return room
+      }
+    }
+    val teleport = Teleport(ch)
+    room.player = teleport
+    teleport.originRoom = room
+    return room
+  }
   // Construct it with the 'Builder' pattern:
   fun build(): Stage {
-    // Step 1: Create the empty rooms:
-/*
-    (0..lines.size).forEach { row ->
-      (0 until lines[0].length).forEach {
-        col -> rooms[Pair(row, col)] = Room()
-      }
-    }
-*/
-    // Step 2: Add players to rooms:
+    // Step 1: Create rooms with players:
     lines.withIndex().forEach { (row, line) ->
       line.withIndex().forEach { (col, char) ->
-//        val room = rooms.getOrDefault(
-//          Pair(row, col), Room())
-        val room = Room()
-        factory2(char, room, robot)
-        rooms[Pair(row, col)] = room
-/*
-        when(val player = factory(char)) {
-          is Robot -> robot.room = room
-          is Teleport -> {
-            player.originRoom = room
-            room.player = player
-            teleports.add(player)
-          }
-          else -> room.player = player
-        }
-*/
+        rooms[Pair(row, col)] = factory(char)
       }
     }
-    // Step 3: Connect the doors
+    // Step 2: Connect the doors
     rooms.forEach { (pair, r) ->
       r.doors.connect(
         pair.first, pair.second, rooms)
     }
-    // Step 4: Connect the Teleport rooms
+    // Step 3: Connect the Teleport rooms
     val teleports = mutableListOf<Teleport>()
     rooms.forEach { (pair, r) ->
       val p = r.player
       if (p is Teleport)
         teleports.add(p)
     }
-    val pairs = teleports
-      .sortedBy {
-        it.target
-      }.zipWithNext()
+    val pairs = teleports.sortedBy {
+      it.target
+    }.zipWithNext()
     for((a, b) in pairs) {
       a.targetRoom = b.originRoom
       b.targetRoom = a.originRoom
