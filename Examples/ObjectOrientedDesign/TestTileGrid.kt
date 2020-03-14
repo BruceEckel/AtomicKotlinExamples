1b102a2105aa0548a2831910d07601cb04a65831
Bruce Eckel <mindviewinc@gmail.com>
2020-03-14 17:56:25 -0600

    Update TestTileGrid.kt

--- Examples/ObjectOrientedDesign/TestTileGrid.kt
+++ Examples/ObjectOrientedDesign/TestTileGrid.kt
@@ -4,14 +4,22 @@ import org.hexworks.zircon.api.*
 import org.hexworks.zircon.api.application.*
 import org.hexworks.zircon.api.data.Size
 import org.hexworks.zircon.api.uievent.*
+import org.hexworks.zircon.api.extensions.*
 
 fun main() {
   val grid = SwingApplications
     .startTileGrid(AppConfig.newBuilder()
       .withDefaultTileset(
-        CP437TilesetResources.rogueYun16x16())
-      .withSize(Size.create(40, 20))
+        TrueTypeFontResources.inconsolata(25))
+//        TrueTypeFontResources.ubuntuMono(25))
+      .withSize(Size.create(60, 30))
       .build())
+  val screen = grid.toScreen()
+  screen.addComponent(Components.label()
+    .withText(stringMaze)
+    .withPosition(5,1))
+  screen.display()
+  screen.theme = ColorThemes.arc()
   grid.handleKeyboardEvents(KeyboardEventType
     .KEY_PRESSED) { event, _ ->
     println("Keyboard event is: $event")
