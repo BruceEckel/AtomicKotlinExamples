febaf7cb51b11338e22296d3276ea54985d05f94
Bruce Eckel <mindviewinc@gmail.com>
2020-03-24 14:16:09 -0600

    Added GenericSafeCast.kt

---
+++ Examples/Generics/GenericSafeCast.kt
@@ -0,0 +1,23 @@
+// Generics/GenericSafeCast.kt
+package generics
+import kotlin.reflect.KClass
+
+inline infix fun <reified T : Any> Any?.to(
+  klass: KClass<T>
+): T? =
+  if (klass.isInstance(this)) this as T
+  else null
+
+inline fun <reified T : Any, R> Any?.to(
+  klass: KClass<T>,
+  convert: T.() -> R? // = { it } ???
+): R? =
+  if (klass.isInstance(this)) {
+    convert(this as T)
+  } else {
+    null
+  }
+
+fun main() {
+  val x: String? = "foo" to String::class
+}
