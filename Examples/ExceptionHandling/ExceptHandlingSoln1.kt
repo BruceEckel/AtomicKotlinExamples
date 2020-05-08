2a9bf88d1ce6bb1624b125beb2841c9d075322f0
Bruce Eckel <mindviewinc@gmail.com>
2020-05-08 10:39:13 -0600

    Exception Handling Exercise 1 is complete

--- Examples/ExceptionHandling/ExceptHandlingSoln1.kt
+++ Examples/ExceptionHandling/ExceptHandlingSoln1.kt
@@ -1,47 +1,91 @@
 // ExceptionHandling/ExceptHandlingSoln1.kt
 package exceptionhandlingsolution1
-import atomictest.*
-import kotlin.random.Random
+import atomictest.Trace                 // StarterCode
+import kotlin.random.Random             // StarterCode
 
-private val trace = Trace()
+private val trace = Trace()             // StarterCode
 
 data class Hamster(val name: String)
 
 open class HamsterCageException(
   val msg: String
-): Exception("HamsterCageException: $msg")
+) : Exception() {
+  override fun toString() =
+    "HamsterCageException: $msg"
+}
 
-class CageFullException(val limit: Int):
+class CageFullException(val limit: Int) :
   HamsterCageException("Cage full > $limit")
 
-class NoSuchHamsterException(val id: String):
+class NoSuchHamsterException(val id: String) :
   HamsterCageException("No Hamster $id")
 
-class OutOfWaterException():
+class OutOfWaterException :
   HamsterCageException("Cage out of water")
 
-class HamsterCage(val capacity: Int) {
+class HamsterCage(val capacity: Int) {   // StarterCode
+  private val seed = Random(47)          // StarterCode
   private val cage = HashSet<Hamster>()
-  private val seed = Random(47)
   fun add(h: Hamster) {
-    if(cage.size >= capacity)
+    if (cage.size >= capacity)
       throw CageFullException(capacity)
     cage.add(h)
   }
   fun get(name: String): Hamster {
-    for(h in cage)
-      if(h.name == name)
+    for (h in cage)
+      if (h.name == name)
         return h
     throw NoSuchHamsterException(name)
   }
-  fun feed() {
-    if((0..10).random(seed) > 8)
+  fun feed(): String {
+    if ((0..10).random(seed) > 8)
       throw OutOfWaterException()
+    return "Feeding complete"
   }
 }
 
-fun main() {
-  trace eq """
+val hamsters = listOf(                 // StarterCode
+  Hamster("Sally"), Hamster("Ralph"),  // StarterCode
+  Hamster("Bob"), Hamster("Sergio"),   // StarterCode
+  Hamster("Allison"), Hamster("Jane")  // StarterCode
+)
 
+fun test(hc: HamsterCage) {
+  try {
+    for (h in hamsters) {
+      trace(h)
+      hc.add(h)
+    }
+  } catch (e: CageFullException) {
+    trace(e)
+  }
+  try {
+    trace(hc.get(hamsters[0].name))
+    trace(hc.get("Morty"))
+  } catch (e: NoSuchHamsterException) {
+    trace(e)
+  }
+  try {
+    trace(hc.feed())
+    trace(hc.feed())
+    trace(hc.feed())
+  } catch (e: OutOfWaterException) {
+    trace(e)
+  }
+}
+
+                    // All of main() is StarterCode:
+fun main() {
+  test(HamsterCage(3))
+  trace eq """
+  Hamster(name=Sally)
+  Hamster(name=Ralph)
+  Hamster(name=Bob)
+  Hamster(name=Sergio)
+  HamsterCageException: Cage full > 3
+  Hamster(name=Sally)
+  HamsterCageException: No Hamster Morty
+  Feeding complete
+  HamsterCageException: Cage out of water
   """
 }
