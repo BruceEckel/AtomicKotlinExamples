224f41edae08116c8b75ee6be14fdb064d03418a
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2020-06-26 19:39:31 +0200

    Moved tests to 'Tests' folder Note that the directory name should correspond directly to the package name ('unittesting)' to make Idea/Gradle find these tests to run them

--- Tests/unittesting/TestLearner.kt
+++ Tests/unittesting/TestLearner.kt
@@ -3,6 +3,7 @@ package unittesting
 import unittesting.Language.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 fun makeLearner(
   id: Int,
@@ -15,8 +16,9 @@ class TestLearner {
   @Test
   fun testLearners() {
     val learner = makeLearner(10, Java)
-    val learners =
-      (1..9).map(::makeLearner)
-    assertEquals("", learner)
+    assertEquals("Test Name 10", learner.name)
+    val learners = (1..9).map(::makeLearner)
+    assertTrue(
+      learners.all { it.lang == Kotlin })
   }
 }
