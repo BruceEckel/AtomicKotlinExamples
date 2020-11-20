fe458906e2439065a1e1c36128e78efad503edc5
Bruce Eckel <BruceTEckel@gmail.com>
2020-11-20 10:23:30 -0700

    Update FilterIsInstance.kt

--- Examples/DownCasting/FilterIsInstance.kt
+++ Examples/DownCasting/FilterIsInstance.kt
@@ -4,10 +4,10 @@ import downcasting.*
 import atomictest.eq
 
 fun main() {
-  val humans1 =
+  val humans1: List<Creature> =
     group.filter { it is Human }
   humans1.size eq 2
-  val humans2 =
+  val humans2: List<Human> =
     group.filterIsInstance<Human>()
   humans2 eq humans1
 }
